051b8a0d216bb213f462a1512a21a9733c35d042
GilbertoDeJesus <gadjs2001@gmail.com>
2020-05-20 20:21:40 -0500

    Se creo la tabla de productos yse asignaron las claves foraneas

---
+++ database/migrations/2020_05_21_005728_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->boolean('meta');
+            $table->string('descripcion');
+            $table->decimal('precio_compra',8,2);
+            $table->decimal('precio_venta',8,2);
+            $table->integer('stock');
+            $table->unsignedBigInteger('unit_id');
+            $table->unsignedBigInteger('provider_id');
+            $table->timestamps();
+
+            $table->foreign('provider_id')->references('id')->on('providers');
+            //$table->foreign('unit_id')->references('id')->on('units');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
