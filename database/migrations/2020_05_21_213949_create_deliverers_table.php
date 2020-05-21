36f80e7602ae0de58c3da6dfe8968e2884787cbb
WonderDarko <wonder.montiel@gmail.com>
2020-05-21 17:13:21 -0500

    creacion de la tabla repartidor

---
+++ database/migrations/2020_05_21_213949_create_deliverers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDeliverersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('deliverers', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre');
+            $table->string('apellidop');
+            $table->string('apellidom');
+            $table->date('fecha_nacimiento');
+            $table->date('fecha_contratacion');
+            $table->string('telfono')->unique();
+            $table->string('email')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('deliverers');
+    }
+}
