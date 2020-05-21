3425342c053e96c8baabeb4800f1983021d28528
WonderDarko <wonder.montiel@gmail.com>
2020-05-21 18:03:21 -0500

    creacion de la tabla gasto

---
+++ database/migrations/2020_05_21_224449_create_expenses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateExpensesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->string('descripcion');
+            $table->double('monto',8,2);
+            $table->unsignedBigInteger('deliverer_id');
+            $table->unsignedBigInteger('expense_type_id');
+            $table->foreign('deliverer_id')->references('id')->on('deliverers');
+            $table->foreign('expense_type_id')->references('id')->on('expense_types');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('expenses');
+    }
+}
