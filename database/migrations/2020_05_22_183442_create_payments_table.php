b82486d46f4bb1a3616c0f2fed9ecbf9a388413b
aguilar9122 <a3519110270@alumno.uttehuacan.edu.mx>
2020-05-22 14:12:36 -0500

    Creacion de las tablas abono y bono

---
+++ database/migrations/2020_05_22_183442_create_payments_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->id();
+            $table->date('fecha');
+            $table->decimal('monto',8,2);
+            $table->unsignedBigInteger('credit_id');
+            $table->timestamps();
+
+            $table->foreign('credit_id')->references('id')->on('credits');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
