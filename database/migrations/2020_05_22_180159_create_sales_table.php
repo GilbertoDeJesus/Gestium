2bd2ec89b264c8e30fbd2f356c819510d43af4ac
WonderDarko <wonder.montiel@gmail.com>
2020-05-22 13:17:32 -0500

    Creacion de la tabla venta

---
+++ database/migrations/2020_05_22_180159_create_sales_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->date('fecha');
+            $table->decimal('monto',8,2);
+            $table->string('observacion');
+            $table->unsignedBigInteger('deliverer_id');
+            $table->unsignedBigInteger('customer_id');
+            $table->timestamps();
+
+            $table->foreign('deliverer_id')->references('id')->on('deliverers');
+            $table->foreign('customer_id')->references('id')->on('customers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
