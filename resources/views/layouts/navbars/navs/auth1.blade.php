d8f22105e7140d4bf6f4e203ecbcd11dceef7eac
GilbertoDeJesus <gadjs2001@gmail.com>
2020-07-24 21:52:00 -0500

    Se cambio el diseno de la tabla del modulo de Ventas

--- resources/views/layouts/navbars/navs/auth1.blade.php
+++ resources/views/layouts/navbars/navs/auth1.blade.php
@@ -4,6 +4,12 @@
     <div class="navbar-wrapper">
       <a class="navbar-brand" href="#"> Aplicación de administración </a>
     </div>
+    <div class="navbar-brand" style="color: #FFFFFF">
+        <?php
+            date_default_timezone_set('America/Mexico_City');
+            echo date("d/m/Y");
+        ?>
+    </div>
     <button class="navbar-toggler" type="button" data-toggle="collapse" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
         <span class="sr-only">Toggle navigation</span>
         <span class="navbar-toggler-icon icon-bar"></span>
